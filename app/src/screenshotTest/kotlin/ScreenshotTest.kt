70f66829149e5e5af3efc413d7191f63617a8049
Matias Isella <matias.isella@glovoapp.com>
2024-09-11 21:09:33 +0200

    Initial commit

---
+++ app/src/screenshotTest/kotlin/ScreenshotTest.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun someTest(){
+    BasicText("ASDASDASDASD")
+}
